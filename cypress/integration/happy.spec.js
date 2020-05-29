24e3e7c999c6c7bc921bb8d295ba1da6c6b7dece
Gregor Martynus <gregor@martynus.net>
2020-05-29 16:45:49 -0700

    ci(test): pass base URL via Cypress.env instead of environment variables

--- cypress/integration/happy.spec.js
+++ cypress/integration/happy.spec.js
@@ -1,6 +1,6 @@
 /// <reference types="Cypress" />
 
-const baseUrl = process.env.TEST_URL || "http://localhost:3000";
+const baseUrl = Cypress.env("base_url") || "http://localhost:3000";
 
 describe("Happy path", () => {
   it("Sends an authenticated `GET /emojis` request", () => {
